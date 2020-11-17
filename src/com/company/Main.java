44a5496fd2bb35c33ec3eb7edfe65d48c50e80b8
karelfila <karel.fila@educanet.cz>
2020-11-17 16:02:29 +0100

    Dělal jsem iffy a return to main menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,16 +37,44 @@ public class Main {
 
             int option2 = Integer.parseInt(sc.nextLine());
 
-            if(option2 == 1) {
+            if (option2 == 1) {
                 System.out.println("Enter the name of the task:");
                 String task = sc.nextLine();
                 System.out.println("--------------------------------------------------------------------");
                 System.out.println("You can ad up to 10 tasks");
                 System.out.println("You have  " + tasks + "/10");
             }
-            if(option2 == 2) {
+            if (option2 == 2) {
+                System.out.println(tasks);
+                System.out.println("What task do you want to update?");
+                int update = sc.nextInt();
+                }
 
+            if (option2 == 3) {
+                System.out.println("What task do you want to delete?");
+                int delete = sc.nextInt();
             }
+            if (option2 == 4);
+
+            System.out.println("  _______        _                                                    \n" +
+                    " |__   __|      | |                                                   \n" +
+                    "    | | __ _ ___| | __    _ __ ___   __ _ _ __   __ _  __ _  ___ _ __ \n" +
+                    "    | |/ _` / __| |/ /   | '_ ` _ \\ / _` | '_ \\ / _` |/ _` |/ _ | '__|\n" +
+                    "    | | (_| \\__ |   <    | | | | | | (_| | | | | (_| | (_| |  __| |   \n" +
+                    "    |_|\\__,_|___|_|\\_\\   |_| |_| |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   \n" +
+                    "                                                       __/ |          \n" +
+                    "                                                      |___/    ");
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("You can save up to 10 tasks!");
+            System.out.println("You have 0/10");
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("1) Manage Tasks");
+            System.out.println("2) Exit");
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("Task managment");
+            System.out.println("--------------------------------------------------------------------");
+
+
         }
     }
 }
