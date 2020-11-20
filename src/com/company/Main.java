bb26d5c059497b6cc41141f1c3b26844c270f8e0
karelfila <karel.fila@educanet.cz>
2020-11-20 13:58:26 +0100

    Progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] tasks = new String[10];
+        int taskcount = 0;
         System.out.println("  _______        _                                                    \n" +
                 " |__   __|      | |                                                   \n" +
                 "    | | __ _ ___| | __    _ __ ___   __ _ _ __   __ _  __ _  ___ _ __ \n" +
@@ -26,10 +27,10 @@ public class Main {
         System.out.println("--------------------------------------------------------------------");
 
         int option = Integer.parseInt(sc.nextLine());
-        if(option == 2) {
+        if (option == 2) {
             return;
         }
-        while(option == 1) {
+        while (option == 1) {
             System.out.println("1) Create a new task");
             System.out.println("2) Update a task");
             System.out.println("3) Delete a task");
@@ -40,41 +41,41 @@ public class Main {
             if (option2 == 1) {
                 System.out.println("Enter the name of the task:");
                 String task = sc.nextLine();
+                tasks[taskcount] = task;
+                taskcount = taskcount + 1;
                 System.out.println("--------------------------------------------------------------------");
                 System.out.println("You can ad up to 10 tasks");
-                System.out.println("You have  " + tasks + "/10");
+                System.out.println("You have  " + taskcount + "/10");
+                System.out.println("--------------------------------------------------------------------");
             }
             if (option2 == 2) {
-                System.out.println(tasks);
+
                 System.out.println("What task do you want to update?");
-                int update = sc.nextInt();
+                for (int i = 0; i < tasks.length; i++) {
+                    System.out.println("#"+ i + " " + tasks[i]);
                 }
+                int update = sc.nextInt();
+            }
 
             if (option2 == 3) {
                 System.out.println("What task do you want to delete?");
                 int delete = sc.nextInt();
             }
-            if (option2 == 4);
+            if (option2 == 4)
+                System.out.println("You can save up to 10 tasks!");
+            System.out.println("You have " + taskcount + "/10");
+            for (int i = 0; i < tasks.length; i++) {
+                System.out.println("#"+ i + " " + tasks[i]);
 
-            System.out.println("  _______        _                                                    \n" +
-                    " |__   __|      | |                                                   \n" +
-                    "    | | __ _ ___| | __    _ __ ___   __ _ _ __   __ _  __ _  ___ _ __ \n" +
-                    "    | |/ _` / __| |/ /   | '_ ` _ \\ / _` | '_ \\ / _` |/ _` |/ _ | '__|\n" +
-                    "    | | (_| \\__ |   <    | | | | | | (_| | | | | (_| | (_| |  __| |   \n" +
-                    "    |_|\\__,_|___|_|\\_\\   |_| |_| |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   \n" +
-                    "                                                       __/ |          \n" +
-                    "                                                      |___/    ");
-            System.out.println("--------------------------------------------------------------------");
-            System.out.println("You can save up to 10 tasks!");
-            System.out.println("You have 0/10");
+            }
             System.out.println("--------------------------------------------------------------------");
             System.out.println("1) Manage Tasks");
             System.out.println("2) Exit");
             System.out.println("--------------------------------------------------------------------");
             System.out.println("Task managment");
             System.out.println("--------------------------------------------------------------------");
-
-
         }
+        tasks[0] = "TVOJE MÁMA";
+
     }
 }
